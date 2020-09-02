29644afd330edd1e9165a96ee0f64c739b513b86
Arthur CARCHI <arthur.carchi@gmail.com>
2020-09-02 17:24:18 +0200

    Adding Bulma and integrating showcase

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Showcase :modelSid="modelSid" /> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Showcase from './components/Showcase'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Showcase,
+  },
+  data: function() {
+    return {
+      modelSid: "SxQL3iGyoDo"
+    }
+  },
+  computed: {
+    getTourUrl: function () {
+      return `https://my.matterport.com/show?m=${this.modelSid}&play=1`
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Showcase.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="showcase-wrapper">
+    <iframe 
+      :src="getTourUrl"
+      frameborder="0" allowfullscreen allow="vr"
+      id="showcase_iframe"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Showcase',
+  props: ['modelSid'],
+  computed: {
+    getTourUrl() {
+      return `https://my.matterport.com/show?m=${this.modelSid}&play=1`
+    }
+  }
+}
+</script>
+
+<style>
+.showcase-wrapper {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 56.25%;
+}
+
+.showcase-wrapper iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+</style>
